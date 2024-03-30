e3cf8b0e8f96108b1c78086d11147331c511c684
Egor <egorpoletaykin@gmail.com>
2024-03-30 17:36:01 +0300

    :sparkles: Sync + GetLastUserChangeID -> Internal object

--- auth.go
+++ auth.go
@@ -100,38 +100,3 @@ func (c *Client) ChangeLevel(ctx context.Context, login string, level Level) (re
 
 	return res.Ok()
 }
-
-func GetLastUserChangeID(ctx context.Context, c *Client) (res gost.Result[uint64]) {
-	r, err := c.cl.GetLastUserChangeID(withAuth(ctx), &api.GetLastUserChangeIDRequest{})
-	if err != nil {
-		return res.Err(errFromGRPCError(err))
-	}
-
-	return res.Ok(r.LastChangeID)
-}
-
-func Sync(ctx context.Context, c *Client, users []Internal_User) (res gost.ResultN) {
-	_, err := c.cl.Sync(withAuth(ctx), &api.SyncData{
-		Users: apiUsersFromInternalUsers(users),
-	})
-
-	if err != nil {
-		return res.Err(errFromGRPCError(err))
-	}
-
-	return res.Ok()
-}
-
-func apiUsersFromInternalUsers(users []Internal_User) []*api.User {
-	var out []*api.User
-	for _, u := range users {
-		out = append(out, &api.User{
-			Id:       uint64(u.ID),
-			Login:    u.Login,
-			Password: u.Password,
-			Level:    uint32(u.Level),
-			Active:   u.Active,
-		})
-	}
-	return out
-}
--- internal.go
+++ internal.go
@@ -2,6 +2,7 @@ package itisadb
 
 import (
 	"context"
+
 	"github.com/egorgasay/gost"
 	api "github.com/egorgasay/itisadb-shared-proto/go"
 )
@@ -24,4 +25,39 @@ func (i *internal) GetRAM(ctx context.Context, c *Client) (res gost.Result[RAM])
 	ram := r.GetRam()
 
 	return res.Ok(RAM{Total: ram.Total, Available: ram.Available})
+}
+
+func (i *internal) GetLastUserChangeID(ctx context.Context, c *Client) (res gost.Result[uint64]) {
+	r, err := c.cl.GetLastUserChangeID(withAuth(ctx), &api.GetLastUserChangeIDRequest{})
+	if err != nil {
+		return res.Err(errFromGRPCError(err))
+	}
+
+	return res.Ok(r.LastChangeID)
+}
+
+func (i *internal) Sync(ctx context.Context, c *Client, users []Internal_User) (res gost.ResultN) {
+	_, err := c.cl.Sync(withAuth(ctx), &api.SyncData{
+		Users: apiUsersFromInternalUsers(users),
+	})
+
+	if err != nil {
+		return res.Err(errFromGRPCError(err))
+	}
+
+	return res.Ok()
+}
+
+func apiUsersFromInternalUsers(users []Internal_User) []*api.User {
+	var out []*api.User
+	for _, u := range users {
+		out = append(out, &api.User{
+			Id:       uint64(u.ID),
+			Login:    u.Login,
+			Password: u.Password,
+			Level:    uint32(u.Level),
+			Active:   u.Active,
+		})
+	}
+	return out
 }
